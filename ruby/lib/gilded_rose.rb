0bc3a0b52bd5d2a18dc065a18ced861a473e6713
Lubos Michalic <lnmichalic@gmail.com>
2017-07-13 22:15:05 +0100

    Edit out all the refactored code and bring back in the original code as the realization that some edge case tests are missing

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -6,97 +6,97 @@ class GildedRose
 
   def update_quality_and_sell_in()
     @items.each do |item|
-      if item.name == 'bread'
-        update_quality_and_sell_in_of_bread(item)
-      end
-      if item.name == 'Aged Brie'
-        update_quality_and_sell_in_of_aged_brie(item)
-      end
-      if item.name == 'Sulfuras, Hand of Ragnaros'
-        update_quality_and_sell_in_of_hand_of_ragnaros(item)
-      end
-      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-        update_quality_and_sell_in_of_backstage_pass(item)
-      end
-
-
-      # if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-      #   if item.quality > 0
-      #     if item.name != "Sulfuras, Hand of Ragnaros"
-      #       item.quality = item.quality - 1
-      #     end
-      #   end
-      # else
-      #   if item.quality < 50
-      #     item.quality = item.quality + 1
-      #     if item.name == "Backstage passes to a TAFKAL80ETC concert"
-      #       if item.sell_in < 11
-      #         if item.quality < 50
-      #           item.quality = item.quality + 1
-      #         end
-      #       end
-      #       if item.sell_in < 6
-      #         if item.quality < 50
-      #           item.quality = item.quality + 1
-      #         end
-      #       end
-      #     end
-      #   end
+      # if item.name == 'bread'
+      #   update_quality_and_sell_in_of_bread(item)
       # end
-      # if item.name != "Sulfuras, Hand of Ragnaros"
-      #   item.sell_in = item.sell_in - 1
+      # if item.name == 'Aged Brie'
+      #   update_quality_and_sell_in_of_aged_brie(item)
       # end
-      # if item.sell_in < 0
-        # if item.name != "Aged Brie"
-        #   if item.name != "Backstage passes to a TAFKAL80ETC concert"
-        #     if item.quality > 0
-        #       if item.name != "Sulfuras, Hand of Ragnaros"
-        #         item.quality = item.quality - 1
-        #       end
-        #     end
-        #   else
-        #     item.quality = item.quality - item.quality
-        #   end
-        # else
-        #   if item.quality < 50
-        #     item.quality = item.quality + 1
-        #   end
-        # end
+      # if item.name == 'Sulfuras, Hand of Ragnaros'
+      #   update_quality_and_sell_in_of_hand_of_ragnaros(item)
       # end
-    end
-  end
+      # if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+      #   update_quality_and_sell_in_of_backstage_pass(item)
+      # end
 
-  def update_quality_and_sell_in_of_bread(item)
-    item.quality -= 1 if item.sell_in < 1
-    item.sell_in -= 1
-    item.quality -= 1
-  end
-  def update_quality_and_sell_in_of_aged_brie(item)
-    if item.quality >= 50
-      item.sell_in -= 1
-      return
-    end
-    if item.sell_in < 1
-      item.quality += 1
-    end
-    item.sell_in -= 1
-    item.quality += 1
-    return
-  end
-  def update_quality_and_sell_in_of_hand_of_ragnaros(item)
-  end
 
-  def update_quality_and_sell_in_of_backstage_pass(item)
-    if item.quality >= 50
-      item.sell_in -= 1
-      return
+      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
+        if item.quality > 0
+          if item.name != "Sulfuras, Hand of Ragnaros"
+            item.quality = item.quality - 1
+          end
+        end
+      else
+        if item.quality < 50
+          item.quality = item.quality + 1
+          if item.name == "Backstage passes to a TAFKAL80ETC concert"
+            if item.sell_in < 11
+              if item.quality < 50
+                item.quality = item.quality + 1
+              end
+            end
+            if item.sell_in < 6
+              if item.quality < 50
+                item.quality = item.quality + 1
+              end
+            end
+          end
+        end
+      end
+      if item.name != "Sulfuras, Hand of Ragnaros"
+        item.sell_in = item.sell_in - 1
+      end
+      if item.sell_in < 0
+        if item.name != "Aged Brie"
+          if item.name != "Backstage passes to a TAFKAL80ETC concert"
+            if item.quality > 0
+              if item.name != "Sulfuras, Hand of Ragnaros"
+                item.quality = item.quality - 1
+              end
+            end
+          else
+            item.quality = item.quality - item.quality
+          end
+        else
+          if item.quality < 50
+            item.quality = item.quality + 1
+          end
+        end
+      end
     end
-    item.quality += 1 if item.sell_in < 6
-    item.quality += 1 if item.sell_in < 11
-    item.quality += 1
-    item.sell_in -= 1
-    return
   end
+  #
+  # def update_quality_and_sell_in_of_bread(item)
+  #   item.quality -= 1 if item.sell_in < 1
+  #   item.sell_in -= 1
+  #   item.quality -= 1
+  # end
+  # def update_quality_and_sell_in_of_aged_brie(item)
+  #   if item.quality >= 50
+  #     item.sell_in -= 1
+  #     return
+  #   end
+  #   if item.sell_in < 1
+  #     item.quality += 1
+  #   end
+  #   item.sell_in -= 1
+  #   item.quality += 1
+  #   return
+  # end
+  # def update_quality_and_sell_in_of_hand_of_ragnaros(item)
+  # end
+  #
+  # def update_quality_and_sell_in_of_backstage_pass(item)
+  #   if item.quality >= 50
+  #     item.sell_in -= 1
+  #     return
+  #   end
+  #   item.quality += 1 if item.sell_in < 6
+  #   item.quality += 1 if item.sell_in < 11
+  #   item.quality += 1
+  #   item.sell_in -= 1
+  #   return
+  # end
 end
 
 class Item
